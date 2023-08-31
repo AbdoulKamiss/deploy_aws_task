03bb1fe748f62215bf2d2097e8d70a37faeecf5b
AbdoulKamiss <abdoulayekamissoko20@gmail.com>
2023-08-31 06:38:17 +0000

    deploy

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -49,7 +49,7 @@
 #
 # The server-based syntax can be used to override options:
 # ------------------------------------
-server "3.83.65.187",
+server "3.95.185.248",
   user: "ec2-user",
   roles: %w{web db app},
   ssh_options: {
